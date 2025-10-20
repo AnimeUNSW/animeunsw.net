b4d84912bbaba5dd2143f44cdb6aab306f96cc96
Andy <andy.ruan2005@gmail.com>
2025-10-21 09:49:56 +1100

    forgot to add the componenets lol

---
+++ src/lib/components/ui/nav/join-social-list.svelte
@@ -0,0 +1,26 @@
+<script>
+  import JoinSocial from "./join-social.svelte";
+</script>
+
+<JoinSocial
+  icon="rubric"
+  label="Rubric"
+  href="https://campus.hellorubric.com/?s=12432"
+  big={true}
+/>
+<JoinSocial icon="discord" label="Discord" href="https://discord.gg/aunsw" />
+<JoinSocial
+  icon="instagram"
+  label="Instagram"
+  href="https://instagram.com/animeunsw"
+/>
+<JoinSocial
+  icon="facebook"
+  label="Facebook"
+  href="https://www.facebook.com/unswanime/"
+/>
+<JoinSocial
+  icon="xiaohongshu"
+  label="RedNote"
+  href="https://discord.gg/aunsw"
+/>
---
+++ src/lib/components/ui/nav/nav-item-list.svelte
@@ -0,0 +1,9 @@
+<script>
+  import NavItem from "./nav-item.svelte";
+  export let scrollY = 0;
+</script>
+
+<NavItem href="/events" label="Events" {scrollY} />
+<NavItem href="/sponsors" label="Sponsors" {scrollY} />
+<NavItem href="/info" label="Info" {scrollY} />
+<NavItem href="/blog" label="Blog" {scrollY} />
